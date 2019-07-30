c1ee8a092f7dc613173a6f48c61e65ddbb0b8839
sbig04 <agnihotris1715@gmail.com>
2019-07-29 22:18:59 -0700

    Initial check in of scala trial

---
+++ Scala_trial/src/classEg.scala
@@ -0,0 +1,17 @@
+
+object classEg {
+  def main(arg: Array[String]){
+    var ob = new NewClass("Hello World!!")
+        var ob1 = new NewClass("Hello India!!")
+
+        var ob2 = new NewClass("Hello Shruti!!")
+
+    ob.sayHi()
+    ob1.sayHi()
+    ob2.sayHi()
+  }
+}
+
+class NewClass(mssg: String){
+  def sayHi() = println(mssg)
+}
