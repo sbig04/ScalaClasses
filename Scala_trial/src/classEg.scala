a51ebfc313b01af10f009ed7a78c8912ad74adcd
sbig04 <agnihotris1715@gmail.com>
2019-08-17 01:20:57 -0700

    minor spacing change

--- Scala_trial/src/classEg.scala
+++ Scala_trial/src/classEg.scala
@@ -2,9 +2,8 @@
 object classEg {
   def main(arg: Array[String]){
     var ob = new NewClass("Hello World!!")
-        var ob1 = new NewClass("Hello India!!")
-
-        var ob2 = new NewClass("Hello Shruti!!")
+    var ob1 = new NewClass("Hello India!!")
+    var ob2 = new NewClass("Hello Shruti!!")
 
     ob.sayHi()
     ob1.sayHi()
